873d792986f2d9b6b0c16ce9e643a24a00b1ddf2
shreyupadhyay <69194500+shreyupadhyay@users.noreply.github.com>
2020-11-16 13:58:43 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:task1/UI/home.dart';
+
+main()
+{
+  runApp(appClass());
+}
+class appClass extends StatelessWidget{
+  Widget build(BuildContext context)
+  {
+    return myApps();
+  }
+}
